5b54628d0dde5a0bad9407a0f0acb4ce323aca15
Ray Klump <raymond.klump@gmail.com>
2019-08-25 21:14:55 -0500

    Added an experimental line.

--- Junk.java
+++ Junk.java
@@ -2,5 +2,6 @@ public class Junk {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("This is pretty cool.");
+		System.out.println("This is risky experimental stuff.");
 	}
 }
